187951b97b3c4799bd96d9301f68a03e9656980e
John-spit <jonathan-spiteri-pro@gmail.com>
2024-05-02 13:41:44 +0200

    Projet fini

--- assets/script.js
+++ assets/script.js
@@ -18,21 +18,36 @@ const slides = [
   },
 ];
 
-let currentSlide = 0;
-const numberOfSlide = slides.length;
+// Variables et constantes nécessaires au projet
 
+const numberOfSlide = slides.length;
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
+const bannerImage = document.querySelector(".banner-img");
+const bannerText = document.querySelector("#banner p");
+let position = 0;
+
+// Création des fonctions sur les flèches
 
 arrowLeft.addEventListener("click", function () {
-  console.log("Click sur la flèche gauche");
+  if (position == 0) {
+    position = numberOfSlide - 1;
+  } else {
+    position--;
+  }
+  updateSlide();
 });
 
 arrowRight.addEventListener("click", function () {
-  console.log("Click sur la flèche droite");
+  if (position == numberOfSlide - 1) {
+    position = 0;
+  } else {
+    position++;
+  }
+  updateSlide();
 });
 
-// Création d'une fonction pour créer les "dots"
+// Création d'une fonction pour créer les "dots" selon le nombre de photos
 
 function creationDots() {
   const dots = document.querySelector(".dots");
@@ -43,4 +58,29 @@ function creationDots() {
   }
 }
 
+// Création d'une fonction pour associer le bon "dot" à l'image sélectionée
+
+function currentDot() {
+  const listDots = document.querySelectorAll(".dot");
+  for (let i = 0; i < listDots.length; i++) {
+    const dot = listDots[i];
+    if (i == position) {
+      dot.classList.add("dot_selected");
+    } else {
+      dot.classList.remove("dot_selected");
+    }
+  }
+}
+
+// Création d'une fonction pour mettre à jour l'image et le texte
+
+function updateSlide() {
+  bannerImage.src = "./assets/images/slideshow/" + slides[position].image;
+  bannerText.innerHTML = slides[position].tagLine;
+  currentDot();
+}
+
+// Exécution des fonctions
+
 creationDots();
+updateSlide();
